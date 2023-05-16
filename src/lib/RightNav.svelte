3343076c5440182965c1a8861c73a3f57cb5ca23
Zedjukez <rubenzylom@gmail.com>
2023-05-16 14:55:38 +0200

    rightnav single component

---
+++ src/lib/RightNav.svelte
@@ -0,0 +1,71 @@
+<script>
+import pfp from '../assets/images/pfp.jpg'
+
+
+
+</script>
+<div class="right-nav">
+    <br>
+     <input type="text" placeholder="Search..">
+    <br>
+    <div class="mightlike">
+      <h1 style="color:black; padding-left:65px;">You might like</h1>
+      <div class="followsuggest">
+        <div class="profiel"><img src="{pfp}"></div>
+        <div class="username"><h2>The cool cat</h2><h3>@TheCoolCat</h3></div>
+        <div class ="followbutton"><button>follow</button></div>
+  
+  
+  
+  
+      </div>
+      
+  
+  
+    </div>
+    
+  
+  
+  </div>
+
+<style>
+    .right-nav input[type=text]{
+    padding:10px;
+    padding-right:200px;
+    padding-top: 5px;
+    margin-left: 45px;
+    border-radius: 50px;
+    background-color: lightgrey;
+  }
+  .mightlike{
+  background-color: lightgrey;
+  width: 350px;
+  height: 350px;
+  border: 15px solid rgb(31, 38, 31);
+  margin: 50px;
+  border-radius: 35px;
+  }
+
+  .followsuggest{
+    display: flex;
+    flex-direction: row;
+    }
+  .profiel img{
+    max-width: 48px;
+    border-radius: 30px;
+  }
+  .profiel{
+    margin-left: 20px;
+  }
+  .username h2,h3{
+    color:black;
+    margin: 0px;
+    padding-left: 20px;
+  }
+  .username h3{
+    color:gray;
+  }
+  .followbutton{
+    padding-left: 20px;
+  }
+  </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,10 @@
     import './+page.css'
     import '../app.css'
     import paw  from '../assets/images/logosite.png'
-    import pfp from '../assets/images/pfp.jpg'
+   
 
   import { navbarButtons } from '../data/routes';
+    import RightNav from '$lib/RightNav.svelte';
 
 </script>
 
@@ -18,29 +19,7 @@
     
 </div>
 
-<div class="right-nav">
-  <br>
-   <input type="text" placeholder="Search..">
-  <br>
-  <div class="mightlike">
-    <h1 style="color:black; padding-left:65px;">You might like</h1>
-    <div class="followsuggest">
-      <div class="profiel"><img src="{pfp}"></div>
-      <div class="username"><h2>The cool cat</h2><h3>@TheCoolCat</h3></div>
-      <div class ="followbutton"><button>follow</button></div>
-
-
-
-
-    </div>
-    
-
-
-  </div>
-  
-
-
-</div>
+<RightNav/>
 
 
 <slot />
@@ -65,45 +44,5 @@
   .active {
     background-color: #100100;
     border-radius: 40px;
-  }
-
-  .right-nav input[type=text]{
-    padding:10px;
-    padding-right:200px;
-    padding-top: 5px;
-    margin-left: 45px;
-    border-radius: 50px;
-    background-color: lightgrey;
-  }
-  .mightlike{
-  background-color: lightgrey;
-  width: 350px;
-  height: 350px;
-  border: 15px solid rgb(31, 38, 31);
-  margin: 50px;
-  border-radius: 35px;
-  }
-
-  .followsuggest{
-    display: flex;
-    flex-direction: row;
-    }
-  .profiel img{
-    max-width: 48px;
-    border-radius: 30px;
-  }
-  .profiel{
-    margin-left: 20px;
-  }
-  .username h2,h3{
-    color:black;
-    margin: 0px;
-    padding-left: 20px;
-  }
-  .username h3{
-    color:gray;
-  }
-  .followbutton{
-    padding-left: 20px;
   }
 </style>
