71cece23fb81d50b41fa3e857bd8575ee3b85576
Zedjukez <rubenzylom@gmail.com>
2023-05-16 12:10:51 +0200

    Logo

---
+++ src/routes/OK/+page.svelte
@@ -0,0 +1,14 @@
+<main>
+    <h1>OPGELET OPGELET</h1>
+    <h2>Jiri Beurskens is verdacht van TWERKEN!!!!!</h2>
+    <h3>Vermijd deze man ten alle kosten!!<img src='../../assets/images/uncanny.png'alt="HEEEEELP"></h3>
+
+
+
+
+
+
+
+
+
+</main>
