9a0e7dd4b9e55cf732571d7da2fe1ddef14aaaea
jiri132 <jiri28beurskens@gmail.com>
2023-05-17 11:25:29 +0200

    Updated explore page

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -1,3 +1,16 @@
+<script lang="ts">
+    import PostViewer from '$lib/Posts/PostViewer.svelte';
+
+    import type { PageData } from './$types'
+
+    export let data: PageData
+</script>
 <main>
-    
+    <PostViewer posts={data.feed}/>
 </main>
+<style lang="scss">
+    main {
+      text-align:start;
+    }
+  </style>
+  
