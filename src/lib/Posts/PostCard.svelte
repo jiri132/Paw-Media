029101016b7123fdee267da42202cdbf279dd9b4
jiri132 <jiri28beurskens@gmail.com>
2023-05-17 11:17:58 +0200

    Added postcard
    to read a post using 1 component

---
+++ src/lib/Posts/PostCard.svelte
@@ -0,0 +1,23 @@
+<script lang=ts>
+    export let title : string = "";
+    export let content : string = "";
+    export let author : string = "";
+    export let authorId : string = "";
+</script>
+
+<div class="container">
+    <a href="/profile/{authorId}">{author}</a>
+    <h2>{title}</h2>
+    <p>{content}</p>
+</div>
+
+<style lang="scss">
+    .container {
+        display: block; 
+        background-color: #111;
+        padding: 10px;
+        border-radius: 15px;
+        margin: 10px 20px 10px 20px; 
+        border: #222222 solid 2px;
+    }
+</style>
