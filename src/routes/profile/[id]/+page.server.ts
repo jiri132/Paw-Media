94a3992466595b3720aa3cb944863db465d37759
jiri132 <jiri28beurskens@gmail.com>
2023-05-17 11:19:42 +0200

    profile slug server file

---
+++ src/routes/profile/[id]/+page.server.ts
@@ -0,0 +1,16 @@
+// src/routes/p/[id]/+page.server.ts
+import db from "$lib/database";
+import type { PageServerLoad } from './$types';
+
+// 1.
+export const load = (async ({ params: { id } }) => {
+    // 2.
+    const user = await db.user.findUnique({
+        where: { name_identifier:id },
+    });
+    const user_posts = await db.post.findMany({
+        where: { author_identifier:id }
+    })
+    // 3.
+    return { user, user_posts };
+}) satisfies PageServerLoad;
