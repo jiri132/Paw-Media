2b8d0b71e5cc5a8d99eb4a689aa5f001b58c47d5
jiri132 <jiri28beurskens@gmail.com>
2023-05-17 11:19:30 +0200

    slug profile page viewer

---
+++ src/routes/profile/[id]/+page.svelte
@@ -0,0 +1,20 @@
+<script lang=ts>
+    import Profile from './Component/Profile.svelte';
+    import type { PageData } from './$types'
+    export let data: PageData
+</script>
+
+
+<main>
+    <Profile 
+        name={data.user?.name_display}
+        identifier={data.user?.name_identifier}
+
+        user_posts={data.user_posts}
+    />
+</main>
+
+
+<style lang="scss">
+
+</style>
